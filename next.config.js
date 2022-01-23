804cd07336afbbb60aaa6cf03bb2e52eb005a358
Paul <54406333+DezareD@users.noreply.github.com>
2022-01-23 18:02:40 +0300

    test
    test

--- next.config.js
+++ next.config.js
@@ -2,6 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
     reactStrictMode: true,
-    assetPrefix: isProd ? '/nextjs-portfolio/' : '',
-    basePath: '/nextjs-portfolio'
+    assetPrefix: ''
 }
